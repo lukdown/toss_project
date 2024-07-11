d724062751d00fcda651a169f05bff04100af260
hsjung <hyunsujung3679@gmail.com>
2024-07-11 14:44:41 +0900

    랜덤이미지 출력

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './css/Home.css'
 import axios from 'axios';
 
@@ -6,31 +6,40 @@ function Home() {
   const [imageBlob, setImageBlob] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const effectRan = useRef(false);
 
   useEffect(() => {
-    const fetchImage = async () => {
-      setIsLoading(true);
-      try {
-        const response = await axios.get('/api/get-image', {
-          responseType: 'blob'
-        });
-        const imageObjectURL = URL.createObjectURL(response.data);
-        setImageBlob(imageObjectURL);
-        setIsLoading(false);
-      } catch (err) {
-        setError('이미지를 불러오는 데 실패했습니다.');
-        setIsLoading(false);
-      }
+    if (effectRan.current === false) {
+      const fetchImage = async () => {
+        setIsLoading(true);
+        try {
+          const response = await axios.get('http://127.0.0.1:8000/random-image-generator', {
+            responseType: 'arraybuffer'
+          });
+          const blob = new Blob([response.data], { type: 'image/jpeg' });
+          const imageObjectURL = URL.createObjectURL(blob);
+          setImageBlob(imageObjectURL);
+          setIsLoading(false);
+        } catch (err) {
+          setError('이미지를 불러오는 데 실패했습니다.');
+          setIsLoading(false);
+        }
+      };
+      fetchImage();
+    }
+    return () => {
+      effectRan.current = true;
     };
+  }, []);
 
-    fetchImage();
-
+  useEffect(() => {
+    // 컴포넌트가 언마운트될 때 URL 객체를 해제합니다.
     return () => {
       if (imageBlob) {
         URL.revokeObjectURL(imageBlob);
       }
     };
-  }, []);
+  }, [imageBlob]);
 
   return (
     <div className="image-container">
