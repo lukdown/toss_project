89da10e21b5f1110e4d25c4524f77bc49de8ff76
Sophie1106 <yoonds714@nate.com>
2024-07-11 23:14:49 +0900

    yds

--- src/pages/category/Correction.js
+++ src/pages/category/Correction.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 
 function Correction() {
   const location = useLocation();
   const { transcription } = location.state || {};
+
+
+  
+
   return (
     <div className="correction">
-      <p>In this picture, I can see a cat and a dog together.</p>
-      <p>The cat appears to <span className="correction-green">be</span> a tabby with <span className="correction-green">brown</span> and white fur, <span className="correction-red">mixed breed</span> <span className="correction-green">puppy</span>.</p>
-      <p>They seem to be <span className="correction-green">in an outdoor setting,</span> not sitting on a <span className="correction-red">comfortable-looking</span> couch.</p>
-      <p><span className="correction-blue">appears to be ~</span> : 안쪽에서 더 자세히 보기</p>
-      <p><span className="correction-blue">mixed breed puppy</span> : 안내는 옮은 이름이지만 하나만 맞습니다</p>
-      <p className="score">점수: 8rfafdafad2/145</p>
       <p>{transcription}</p>
-      <p>testdi</p>
     </div>
   );
 }
