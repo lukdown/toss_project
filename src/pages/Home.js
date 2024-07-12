e984ee32fccda5bfc42506b998073c728705df8c
hsjung <hyunsujung3679@gmail.com>
2024-07-12 10:08:08 +0900

    메인 페이지 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
   const [loading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const effectRan = useRef(false);
-  
+
   const [showContent, setShowContent] = useState(false);
   const [audioId, setAudioId] = useState(null);
   const [text, setText] = useState('');
@@ -21,29 +21,38 @@ function Home() {
         try {
           const response = await axios.get('http://127.0.0.1:8000/random-image-generator', {
             responseType: 'arraybuffer',
-            maxRedirects: 0,  // 리다이렉트 방지
+            maxRedirects: 0,
             validateStatus: function (status) {
-              return status >= 200 && status < 300; // 기본값
+              return status >= 200 && status < 300;
             }
           });
           
-          const blob = new Blob([response.data], { type: 'image/jpeg' });
+          const blob = new Blob([response.data], { type: 'image/png' });
           const imageObjectURL = URL.createObjectURL(blob);
           setImageBlob(imageObjectURL);
 
-          setText("안녕하세요?")
+          // formData 생성 및 이미지 blob 추가
+          const formData = new FormData();
+          const file = new File([blob], 'generated_image.png', { type: 'image/png' });
+          formData.append('file', file);
+
+          // formData를 API 엔드포인트로 전송
+          const discription = await axios.post('http://127.0.0.1:8000/image_description/', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          });
+
+          setText(discription.data);
           setIsLoading(false);
           setShowContent(true);
         } catch (err) {
           console.error('Error fetching image:', err);
           if (err.response) {
-            // 서버가 2xx 범위를 벗어나는 상태 코드로 응답한 경우
             setError(`서버 에러: ${err.response.status}`);
           } else if (err.request) {
-            // 요청이 전송되었지만 응답을 받지 못한 경우
             setError('서버로부터 응답이 없습니다.');
           } else {
-            // 요청 설정 중에 오류가 발생한 경우
             setError('요청 설정 중 오류가 발생했습니다.');
           }
           setIsLoading(false);
@@ -68,7 +77,7 @@ function Home() {
     <div className="home-image-container">
       <div className="home-image-box">
         {loading ? (
-          <div>이미지 로딩 중...</div>
+          <div></div>
         ) : error ? (
           <div>{error}</div>
         ) : (
@@ -76,7 +85,7 @@ function Home() {
         )}
       </div>
 
-      {loading && (  
+      {loading && (
         <div className="home-loading">
           <div className="home-loading-spinner"></div>
           <p>로딩 중...</p>
