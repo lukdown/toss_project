e16ff4c235f12a10cc53d9a973e20776ea9b2165
yys <lukdown10@gmail.com>
2024-07-13 05:02:32 +0900

    yys

--- src/pages/TossRecommendation.js
+++ src/pages/TossRecommendation.js
@@ -75,7 +75,13 @@ function TossRecommendation() {
       {loading && (
         <div className="loading">
           <div className="loading-spinner"></div>
-          <p>로딩 중...</p>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <line x1="22" y1="2" x2="11" y2="13"></line>
+              <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+            </svg>
+          </button>
+          {/* <p>로딩 중...</p> */}
         </div>
       )}
 
